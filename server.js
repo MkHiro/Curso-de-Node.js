f7fb9c04510b0dde4ec5cd19d1a4a6d27e4707dd
Pedro G <pedrohirobuy@gmail.com>
2023-01-12 19:19:23 -0300

    Clase 8

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})); 
 app.use(router);
+
 
 router.get('/message', function(req,res){
      res.send('Lista de mensajes');
 });
 router.delete('/message', function(req,res){
-     res.send('Mensaje eliminado');
+     console.log(req.query);
+     console.log(req.body);
+     res.send('Mensaje ' + req.body.text + ' añadido correctamente')
 });
 
 // app.use('/', function(req,res){
