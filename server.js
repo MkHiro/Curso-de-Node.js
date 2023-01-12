40cbe1aa9c44facfe0cf4523285536116eee266a
Pedro G <pedrohirobuy@gmail.com>
2023-01-12 17:08:35 -0300

    Clase 7

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(req,res){
-     res.send('hola');
+app.use(router);
+
+router.get('/message', function(req,res){
+     res.send('Lista de mensajes');
 });
+router.delete('/message', function(req,res){
+     res.send('Mensaje eliminado');
+});
+
+// app.use('/', function(req,res){
+//      res.send('hola');
+// });
 
 app.listen(3000);
 console.log('La app esta escuchando en http://localhost:3000');
