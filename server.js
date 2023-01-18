b0dd08a3a9061719f9aea3ba767e25203b8f6736
Pedro G <pedrohirobuy@gmail.com>
2023-01-18 16:18:29 -0300

    Clase 13

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ router.get('/message', function(req,res){
      });
      response.success(req,res,'Lista de mensajes');
 });
+
 router.post('/message', function(req,res){
      console.log(req.query);
      if(req.query.error == "ok"){
-          response.error(req,res,'Error simulado',400);
+          response.error(req,res,'Error inesperado',500,'Es solo una simulacion de los errores');
      }else{
           response.success(req,res,'Creado correctamente',201);
      }
