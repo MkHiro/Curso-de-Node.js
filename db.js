9a07dddd36803ae141662c1196783a19b60f5264
Pedro G <pedrohirobuy@gmail.com>
2023-02-07 14:40:53 -0300

    Clase 25 + DotEnv

---
+++ db.js
@@ -0,0 +1,15 @@
+const db = require('mongoose');
+
+db.Promise = global.Promise;
+async function connect(url,dbName){
+    db.set('strictQuery', false);
+    //mongodb+srv://<DB_USER><DB_PASS>@<DB_HOST>/?retryWrites=true&w=majority      
+    await db.connect( url, {       //formato MongoDB 5.0
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: dbName
+    });
+    console.log('[db] Conectada con exito a telegrom');
+}
+
+module.exports = connect;
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const db = require('./db');
 const router = require('./network/routes');
+//npm i dotenv, esta libreria lee el archivo .env, subire un .env.example para que puedan alimentar su base de datos.
+require('dotenv').config({ path: ".env" });
+const urlMongo = {
+    dbHost : process.env.DB_HOST,                     
+    dbName : process.env.DB_NAME,                       
+    dbUser : encodeURIComponent(process.env.DB_USER),
+    dbPass : encodeURIComponent(process.env.DB_PASS)
+};
+const url = `mongodb+srv://${urlMongo.dbUser}:${urlMongo.dbPass}@${urlMongo.dbHost}/?retryWrites=true&w=majority`;//formato MongoDB 5.0
+db(url,urlMongo.dbName);
 
 var app = express();
 app.use(bodyParser.json());
